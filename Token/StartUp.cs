3b0e7fb528c4ed4e766a09c5d55b929447ab0f5c
nadia <nadia29092003@gmail.com>
2022-02-07 23:10:04 +0300

    Token

---
+++ Token/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Token
+{
+    public class StartUp
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                           
+                            ValidateIssuer = true,
+                            
+                            ValidIssuer = AuthOptions.ISSUER,
+
+                            
+                            ValidateAudience = true,
+                            
+                            ValidAudience = AuthOptions.AUDIENCE,
+                           
+                            ValidateLifetime = true,
+
+                            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                            
+                            ValidateIssuerSigningKey = true,
+                        };
+                    });
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
